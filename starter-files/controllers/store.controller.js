247c10941d7fdacd442e4ed0cb3b56c926ad1348
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-03 17:36:48 -0600

    Add post route to create store

--- starter-files/controllers/store.controller.js
+++ starter-files/controllers/store.controller.js
@@ -1,7 +1,14 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -5,5 +5,6 @@ const StoreController = require('../controllers/store.controller');
 // Do work here
 router.get('/', StoreController.homePage);
 router.get('/add', StoreController.addStore);
+router.post('/add', StoreController.createStore);
 
 module.exports = router;
