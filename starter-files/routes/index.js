29ce6210e440b15cfa1c77a427cedc412a348b52
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-03 17:51:03 -0600

    Handle errors in createStore

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const StoreController = require('../controllers/store.controller');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', StoreController.homePage);
 router.get('/add', StoreController.addStore);
-router.post('/add', StoreController.createStore);
+router.post('/add', catchErrors(StoreController.createStore));
 
 module.exports = router;
