0447314e9f667fe0c65a34b99d02ac248f3e4ed0
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-03 23:56:47 -0600

    Add route for edit stores

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -7,6 +7,8 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(StoreController.getStores));
 router.get('/stores', catchErrors(StoreController.getStores));
+router.get('/stores/:id/edit', catchErrors(StoreController.editStore));
+
 router.get('/add', StoreController.addStore);
 router.post('/add', catchErrors(StoreController.createStore));
 
