934058b13a5b9ff5172c308b57fcd85a1f7ecedc
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-04 13:06:47 -0600

    Complete search input

--- starter-files/controllers/store.controller.js
+++ starter-files/controllers/store.controller.js
@@ -113,7 +113,13 @@ exports.searchStores = async (req, res) => {
     $text: {
       $search: req.query.q
     }
-  });
+  }, {
+      score: { $meta: 'textScore' }
+    })
+    .sort({
+      score: { $meta: 'textScore' }
+    })
+    .limit(5);
 
   res.json(stores);
 };
