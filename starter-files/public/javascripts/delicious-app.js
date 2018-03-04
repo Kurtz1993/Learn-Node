e16ca99aeb37516e5dee73b4e886b96d8386fc71
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-04 13:33:58 -0600

    Add search box logic

--- starter-files/public/javascripts/delicious-app.js
+++ starter-files/public/javascripts/delicious-app.js
@@ -2,5 +2,7 @@ import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
 import autocomple from './modules/autocomple';
+import typeAhead from './modules/typeAhead';
 
 autocomple($('#address'), $('#lat'), $('#lng'));
+typeAhead($('.search'));
