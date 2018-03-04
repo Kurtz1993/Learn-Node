0a16a1ed9b3f3ad1ed684db247fc4ff7cc653b53
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-04 16:34:03 -0600

    Add heart ajax handler

--- starter-files/public/javascripts/delicious-app.js
+++ starter-files/public/javascripts/delicious-app.js
@@ -4,7 +4,12 @@ import { $, $$ } from './modules/bling';
 import autocomple from './modules/autocomple';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
+import ajaxHeart from './modules/heart';
 
 autocomple($('#address'), $('#lat'), $('#lng'));
 typeAhead($('.search'));
 makeMap($('#map'));
+
+const heartForms = $$('form.heart');
+
+heartForms.on('submit', ajaxHeart);
