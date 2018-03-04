55335b26a19b4e00c24152dd0bb9ef185b093823
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-04 16:03:49 -0600

    Add map handler

--- starter-files/public/javascripts/delicious-app.js
+++ starter-files/public/javascripts/delicious-app.js
@@ -3,6 +3,8 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autocomple from './modules/autocomple';
 import typeAhead from './modules/typeAhead';
+import makeMap from './modules/map';
 
 autocomple($('#address'), $('#lat'), $('#lng'));
 typeAhead($('.search'));
+makeMap($('#map'));
