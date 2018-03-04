15b855d8e3702eea9eee64d68363402219395852
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-04 12:43:47 -0600

    Populate author

--- starter-files/controllers/store.controller.js
+++ starter-files/controllers/store.controller.js
@@ -40,6 +40,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await (new Store(req.body)).save();
 
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
@@ -53,10 +54,18 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)) {
+    throw Error('You must own a store in order to edit it!');
+  }
+}
+
 exports.editStore = async (req, res) => {
   const { id } = req.params;
 
   const store = await Store.findById(id);
+
+  confirmOwner(store, req.user);
 
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
@@ -78,7 +87,8 @@ exports.updateStore = async (req, res) => {
 
 exports.getStoreBySlug = async (req, res, next) => {
   const { slug } = req.params;
-  const store = await Store.findOne({ slug });
+  const store = await Store.findOne({ slug })
+    .populate('author');
 
   if (!store) return next();
 
