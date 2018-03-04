b41072b49eeef161648de2e6b07963939a807e9c
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-03 23:43:08 -0600

    Add stores route

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -5,7 +5,8 @@ const StoreController = require('../controllers/store.controller');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', StoreController.homePage);
+router.get('/', catchErrors(StoreController.getStores));
+router.get('/stores', catchErrors(StoreController.getStores));
 router.get('/add', StoreController.addStore);
 router.post('/add', catchErrors(StoreController.createStore));
 
