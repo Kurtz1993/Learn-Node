fc30080e69b7f5818b80a631837bb0522b05a54e
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-03-05 21:18:33 -0600

    Add reviews as a virtual populate

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -2,54 +2,60 @@ const mongoose = require('mongoose');
 const slug = require('slugs');
 const { Schema } = mongoose;
 
-const storeSchema = Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: 'Please enter a store name.'
-  },
-  slug: String,
-  description: {
-    type: String,
-    trim: true
-  },
-  tags: [String],
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  location: {
-    type: {
+const storeSchema = Schema(
+  {
+    name: {
       type: String,
-      default: 'Point'
+      trim: true,
+      required: 'Please enter a store name.',
     },
-    coordinates: [
-      {
-        type: Number,
-        required: 'You must supply coordinates.'
-      }
-    ],
-    address: {
+    slug: String,
+    description: {
       type: String,
-      required: 'You must supply an address.'
-    }
+      trim: true,
+    },
+    tags: [String],
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    location: {
+      type: {
+        type: String,
+        default: 'Point',
+      },
+      coordinates: [
+        {
+          type: Number,
+          required: 'You must supply coordinates.',
+        },
+      ],
+      address: {
+        type: String,
+        required: 'You must supply an address.',
+      },
+    },
+    photo: String,
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: 'You must supply an author',
+    },
   },
-  photo: String,
-  author: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: 'You must supply an author'
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-});
+);
 
 // Define indexes
 storeSchema.index({
   name: 'text',
-  description: 'text'
+  description: 'text',
 });
 
 storeSchema.index({
-  location: '2dsphere'
+  location: '2dsphere',
 });
 
 storeSchema.pre('save', async function(next) {
@@ -71,8 +77,15 @@ storeSchema.statics.getTagsList = function getTagsList() {
   return this.aggregate([
     { $unwind: '$tags' },
     { $group: { _id: '$tags', count: { $sum: 1 } } },
-    { $sort: { count: -1 } }
+    { $sort: { count: -1 } },
   ]);
 };
+
+// Find reviews where the sotres _id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // What model will be linked?
+  localField: '_id', // Which field on the Store mdoel?
+  foreignField: 'store', // Which field in the Review model?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
